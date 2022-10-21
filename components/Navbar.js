c4009b96c30513bebf528b42522d023b575539db
Luca Roost <luca.roost@bluewin.ch>
2022-10-21 19:16:29 +0200

    login tool

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
           {!user && <li onClick={login} className="btn">Login/Signup</li>}
-          {user && <li>{user.email}</li>}
+          {user && <li>{user.user_metadata.full_name}</li>}
           {user && <li onClick={logout} className="btn">Logout</li>}
         </ul>}
       </nav>
